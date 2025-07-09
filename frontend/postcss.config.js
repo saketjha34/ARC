ac75aaba6468bfdce37299dab7b332b7bc307f20
Saket Jha <148564188+saketjha34@users.noreply.github.com>
2025-07-09 09:35:45 +0530

    Fix PostCSS configuration

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,8 +1,9 @@
 import tailwindcss from '@tailwindcss/postcss'
+import autoprefixer from 'autoprefixer'
 
 export default {
-  plugins: {
-    tailwindcss: tailwindcss({}),
-    autoprefixer: {},
-  },
+  plugins: [
+    tailwindcss({}),
+    autoprefixer(),
+  ],
 }
